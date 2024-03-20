1acd04465ece0f7d4287883bdfe4e5396ccdcb67
NikitaPas <86974012+NikitaPas@users.noreply.github.com>
2024-03-20 08:52:31 +0300

    lab 2 added

---
+++ lab2/components/AssetExample.js
@@ -0,0 +1,57 @@
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+   <TouchableOpacity
+        style={styles.button}
+        onPress={() => console.log("Кнопка нажата")}
+      >
+        <Text style={styles.buttonText}>Новости</Text>
+      </TouchableOpacity>
+      <Image style={styles.logo} source={require('../assets/collab-media-g5wo-_XOzJI-unsplash-600x400.jpg')} />
+      <Text style={styles.paragraph}>
+        Зона гениальности: как туда попасть и там остаться
+      </Text>
+      <Text style={styles.text}>
+      Гениальность — это не удел избранных, а часть каждого из нас. Раскрываем и наслаждаемся!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 15,
+  },
+  paragraph: {
+    marginBottom: 25,
+    marginTop: 25,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+  logo: {
+    alignSelf: 'flex-start',
+    height: 200,
+    width: 280,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'left',
+    marginBottom: 35,
+  },
+  button: {
+    paddingTop: 40,
+    marginBottom: 40,
+    alignSelf: 'flex-start',
+    paddingBottom: 15,
+    
+  },
+  buttonText: {
+    fontSize: 15,
+    color: 'blue',
+  },
+});
